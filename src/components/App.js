3a34e7ca546109f4f6ff5853cd558948b3e47828
garritfra <garritfranke@gmail.com>
2020-01-15 22:08:37 +0100

    Add change color form

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import Button from "react-bootstrap/Button";
+import BSForm from "react-bootstrap/Form";
+
+const Form = styled(BSForm)`
+  margin-top: 1em;
+  margin-left: 1em;
+  width: 10em;
+`;
 
 export default function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Button>Hello World</Button>
+      <Form>
+        <Form.Group controlId="formChangeColor">
+          <Form.Label>Change Color</Form.Label>
+          <Form.Control type="text" placeholder="#f0f0f0"></Form.Control>
+        </Form.Group>
+        <Button>Submit</Button>
+      </Form>
     </div>
   );
 }
